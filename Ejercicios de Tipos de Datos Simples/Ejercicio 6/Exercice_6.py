d3329200e97880dafa0bfc0c308420817bc1e9bf
Axel Ian Solorzano Castañon <83233607+axelsolorzano@users.noreply.github.com>
2024-09-30 21:10:53 -0600

    Create Exercice_6.py

---
+++ Ejercicios de Tipos de Datos Simples/Ejercicio 6/Exercice_6.py
@@ -0,0 +1,6 @@
+#Escribir un programa que lea un entero positivos N introducido por el usuario y 
+#después muestre en pantalla la suma de todos los enteros desde 1 hasta N La suma de los 
+#primeros enteros positivos puede ser calculada de la siguiente forma: suma=N(N+1)/2
+number = int(input("Give me a number"));
+sum = (number*(number+1)/2);
+print("The Sum of all of positives numbers is:",sum);
